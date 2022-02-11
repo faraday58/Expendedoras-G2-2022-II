3b356727e8eb87f56f855df49e078cab191e9316
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-02-11 12:51:15 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Expendedoras_G2_2022_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objeto de la Clase
+            Expendedora miExpendedora = new Expendedora();
+            Console.ReadLine();
+        }
+    }
+}
